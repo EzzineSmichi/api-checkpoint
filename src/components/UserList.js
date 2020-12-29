7f6d6fb8599ff78f60e3f337c67ac4ac2b7db977
EzzineSmichi <ezzinesmichi2@gmail.com>
2020-12-29 20:41:55 +0100

    getting-data-from-api

---
+++ src/components/UserList.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { apiCall } from '../redux/user/action'
+import User from './User'
+
+const UserList = () => {
+
+    const state = useSelector(state => state)
+    const dispatch = useDispatch()
+    console.log(state)
+
+    useEffect(() => {
+        dispatch(apiCall())
+        
+    }, [dispatch])
+
+  
+    return (
+        <div className='container'>
+            {
+                state.isLoading ? (
+                    <h1 style={{textAlign:'center', color:'red'}}>Loading...</h1>
+                )
+                :state.error ? (
+                    <Alert color='danger'>{state.error}</Alert>
+                )
+                :(
+                    state.users.map(user=>{
+                       return(
+                           <User key={user.id} user={user}/>
+                       ) 
+                    })
+                )
+            }
+        </div>
+    )
+}
+
+export default UserList
