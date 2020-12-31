513e11bcb703998bd545ec0209c492dffa7ab262
EzzineSmichi <ezzinesmichi2@gmail.com>
2020-12-31 15:23:41 +0100

    updating

--- src/components/SelektonComponent.js
+++ src/components/SelektonComponent.js
@@ -3,20 +3,18 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 const SelektonComponent = () => {
     return (
-        <div>
-            <SkeletonTheme color="#202020" highlightColor="#444">
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-                <Skeleton animation="wave" variant="rect" width={270} height={200}/>
-
-            </SkeletonTheme>  
+        <div className='container'>
+        {
+            [1,2,3,4,5,6,7,8,9].map(()=>{
+                return (
+                <  >
+                    <SkeletonTheme color="#202020" highlightColor="#444" >
+                        <Skeleton animation="wave" variant="rect" width={350} height={330} style={{margin:'1rem'}}/>
+                    </SkeletonTheme>  
+                        </>
+                 )
+            })
+        }    
         </div>
     )
 }
